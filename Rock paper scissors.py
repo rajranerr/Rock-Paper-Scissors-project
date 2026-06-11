91d3bc787b7a8da635cacb864d8a4e2e4e42b1f0
rajranerr <raneraj71@gmail.com>
2026-06-11 16:44:46 +0530

    add project code

---
+++ Rock paper scissors.py
@@ -0,0 +1,15 @@
+import random
+item_list = ["Rock", "Paper", "Scissor"]
+
+user_choice = input("Enter your move: Rock, Paper, Scissor= ")
+comp_coice = random.choice(item_list)
+
+print(f"User choice = {user_choice}, Computer choice = {comp_coice}")
+
+if user_choice == comp_coice:
+    print("Match is tie")
+elif user_choice == "Rock":
+    if comp_coice == "Paper":
+        print("Computer is win")
+    else:
+        print("You win")
